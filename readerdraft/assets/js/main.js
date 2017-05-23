5592c0360e3240ad274a829249bce3b922b9db52
yunyixie <yxie89@student.monash.edu>
2017-05-24 01:15:37 +1000

    reader

---
+++ readerdraft/assets/js/main.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+
+
+  $('.number').mouseenter(function() {
+    $('.footnote').addClass('showme');
+  });
+
+  $('.number').mouseleave(function() {
+      $('.footnote').removeClass('showme');
+    });
+
+  $('.thumbnail').click(function() {
+    $(this).toggleClass('enlarge');
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  $('.box').addClass('red');
+
+});
