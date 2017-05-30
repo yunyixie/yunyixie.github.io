12f410f00ea61bad973eec966b8ef050dbdf2030
yunyixie <yxie89@student.monash.edu>
2017-05-30 21:16:51 +1000

    game modes update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,65 @@
 $(document).ready(function() {
-
-
   /* navi title */
 
-
-  $('.navi__p').mouseenter(function() {
-    $('.pt').addClass('showme');
+  $(".navi__p").mouseenter(function() {
+    $(".pt").addClass("showme");
   });
 
-  $('.navi__p').mouseleave(function() {
-      $('.pt').removeClass('showme');
-    });
+  $(".navi__p").mouseleave(function() {
+    $(".pt").removeClass("showme");
+  });
 
-    $('.navi__n').mouseenter(function() {
-      $('.nt').addClass('showme');
-    });
+  $(".navi__n").mouseenter(function() {
+    $(".nt").addClass("showme");
+  });
 
-  $('.navi__n').mouseleave(function() {
-      $('.nt').removeClass('showme');
-    });
+  $(".navi__n").mouseleave(function() {
+    $(".nt").removeClass("showme");
+  });
 
-    /*game modes*/
+  /*game modes*/
+  /*body*/
+  $(".switch1").click(function() {
+    $("body").removeClass("hard");
+    $("body").removeClass("hell");
+    $("body").addClass("normal");
+  });
 
-    $('.switch1').click(function() {
-      $('.content').addClass('normal');
-    });
+  $(".switch2").click(function() {
+    $("body").removeClass("normal");
+    $("body").removeClass("hell");
+    $("body").addClass("hard");
+  });
 
+  $(".switch3").click(function() {
+    $("body").removeClass("normal");
+    $("body").removeClass("hard");
+    $("body").addClass("hell");
+  });
 
+  /*content*/
+  $(".switch1").click(function() {
+    $(".content").removeClass("hard");
+    $(".content").removeClass("hell");
+    $(".content").addClass("normal");
+  });
 
+  $(".switch2").click(function() {
+    $(".content").removeClass("normal");
+    $(".content").removeClass("hell");
+    $(".content").addClass("hard");
+  });
 
+  $(".switch3").click(function() {
+    $(".content").removeClass("normal");
+    $(".content").removeClass("hard");
+    $(".content").addClass("hell");
+  });
 
+/*fade*/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  $('.box').addClass('red');
+  $(function() {
+    $('body').removeClass('fade-out');
+});
 
 });
